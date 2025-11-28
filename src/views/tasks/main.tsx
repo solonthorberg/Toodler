c18fb497658063f2af6d8850d180b8ef69ad3f69
solonthorberg <solonhelgason@gmail.com>
2025-11-28 11:30:07 +0000

    fix

--- src/views/tasks/main.tsx
+++ src/views/tasks/main.tsx
@@ -1,7 +1,7 @@
 import AddButton from "@/src/components/buttons/addButton";
 import HeaderAddButton from "@/src/components/buttons/headerAddButton";
-import TaskMoveCard from "@/src/components/cards/TaskMoveCard/TaskMoveCard";
 import TaskCard from "@/src/components/cards/taskCard/taskCard";
+import TaskMoveCard from "@/src/components/cards/taskMoveCard/taskMoveCard";
 import TaskForm from "@/src/components/forms/taskForm";
 
 import { listService } from "@/src/services/listService";
